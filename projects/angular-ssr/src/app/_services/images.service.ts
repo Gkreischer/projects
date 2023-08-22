0b8a70f4e096baeb3ae29a4c6319f94cd6ca5996
Gustavo Kreischer de Almeida <gustavokreischer@gmail.com>
2023-08-21 23:35:22 -0300

    add: pagination

--- projects/angular-ssr/src/app/_services/images.service.ts
+++ projects/angular-ssr/src/app/_services/images.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, BehaviorSubject } from  'rxjs';
+import { tap, BehaviorSubject, catchError, Observable } from  'rxjs';
 import { Image } from '../_models/Image';
+import { ErrorService } from './error.service';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,20 +12,26 @@ export class ImagesService {
   imagesSubject = new BehaviorSubject<Image[]>([]);
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private errorService: ErrorService
   ) { }
 
   get images() {
     return this.imagesSubject.asObservable();
   }
 
-  get() {
-    return this.http.get<[]>('https://picsum.photos/v2/list').pipe(
+  get(term?: string) : Observable<Image[]> {
+
+    term = term?.trim();
+
+    const options = term ? { params: new HttpParams().set('page', term) } : {};
+    return this.http.get<[]>('https://picsum.photos/v2/list', options).pipe(
       tap(
         (res) => {
           return this.imagesSubject.next(res);
         }
-      )
+      ),
+      catchError(this.errorService.handleError)
     );
   }
 }
--- projects/angular-ssr/src/app/app.component.ts
+++ projects/angular-ssr/src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent implements OnInit {
 
 
   images!: Observable<Image[]>;
+  pageNumber: number = 1;
 
   constructor(private serviceImages: ImagesService) {
 
@@ -23,8 +24,31 @@ export class AppComponent implements OnInit {
 
   getImages() {
     this.serviceImages.get().subscribe((res) => {
+      console.log(res)
       this.images = this.serviceImages.images;
     })
+  }
+
+  previousPage() {
+
+    if(this.pageNumber === 1) return;
+
+    this.pageNumber--;
+    this.serviceImages.get((this.pageNumber).toString()).subscribe((res) => {
+      this.images = this.serviceImages.images;
+    })
+  }
+
+  nextPage() {
+    this.pageNumber++;
+    console.log(this.pageNumber)
+    this.serviceImages.get((this.pageNumber).toString()).subscribe((res) => {
+      this.images = this.serviceImages.images;
+    })
+  }
+
+  trackByImage(index: number, image: Image) {
+    return image.id;
   }
 
   title = 'angular-ssr';
